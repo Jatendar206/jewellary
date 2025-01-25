e1c58fe9de1eddbdd8edf0c60022f7924e874573
chanderraja4 <126194171+chanderraja4@users.noreply.github.com>
2025-01-25 21:20:33 +0500

    my jewellay website

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+
+// echo(" ab clear hai");
+//    table in php
+// function one($s,$e,$t){
+// for ($i=$s;$i<=$e;$i++){
+// echo $t. " x ".$i." = ".($t*$i)."<br>";
+// }
+// }
+// one( 1 ,10 ,2);
+// echo"------------------";
+// one( 1 ,10 ,3);
+// echo"------------------";
+// one( 1 ,10 ,4);
+// echo"------------------";
+// one( 1 ,10 ,5);
+// echo"------------------ <br>";
+// one( 1 ,10 ,6);
+// echo"------------------";
+// one( 1 ,10 ,7);
+// echo"------------------";
+// one( 1 ,10 ,8);
+// echo"------------------";
+// one( 1 ,10 ,9);
+// echo"------------------";
+// one( 1 ,10 ,10);
+// echo"------------------";
+// one( 1 ,10 ,11);
+// echo"------------------";
+// one( 1 ,10 ,12);
+// echo"------------------";
+// one( 1 ,10 ,13);
+// echo"------------------";
+// one( 1 ,10 ,14);
+// echo"------------------";
+// one( 1 ,10 ,15);
+// echo"------------------";
+// one( 1 ,10 ,16);
+// echo"------------------";
+// one( 1 ,10 ,20);
+
+// function add($a,$b=90){
+// return($a+$b);
+// };
+// echo (add(30,666));
+
+// datatypes
+$a = 123;
+$b = 12.11;
+$c = 1 >= 1;
+$d = " attack";
+$e = null;
+$n = [10303030300];
+
+// var_dump($a, $b, $c, $d, $e, $n);
+var_dump($a);
+echo "<br> <br>";
+var_dump(  $c);
+echo"<br> <br>";
+var_dump(  $b);
+echo "<br> <br>";
+var_dump(  $d);
+echo "<br> <br> ";
+var_dump(  $e);
+echo "<br> <br> ";
+var_dump($n);
+echo "<br> <br> ";
+    //  constant variable
+define("a", "pakistan india karachi land <br> web development course ");
+echo a;
+define("b","nvgcfxzdzdzxxzd");
+echo b;
---
+++ javascript.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#empty").click(function () {
+        $("#teen").empty();
+    });
+    $("#remove").click(function () {
+        $("#teen").remove();
+    });
+});
